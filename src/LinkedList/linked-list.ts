1426181961ebb184ad6240f5ae939709a6d0b33a
tushar_verma <vermatushar0101@gmail.com>
2023-10-11 18:02:16 +0530

    Update linked-list.ts (#33)
    ISSUE #25
    
    feat: implement forEach() method for linked-list.ts
    
    Co-authored-by: Samyabrata Maji <116789799+samyabrata-maji@users.noreply.github.com>

--- src/LinkedList/linked-list.ts
+++ src/LinkedList/linked-list.ts
@@ -1,121 +1,122 @@
 export class LinkedListNode<T> {
-	value: T;
-	prev: LinkedListNode<T> | null;
-	next: LinkedListNode<T> | null;
+    value: T;
+    prev: LinkedListNode<T> | null;
+    next: LinkedListNode<T> | null;
 
-	constructor(
-		value: T,
-		next: LinkedListNode<T> | null = null,
-		prev: LinkedListNode<T> | null = null,
-	) {
-		this.value = value;
-		this.next = next;
-		this.prev = prev;
-	}
+    constructor(
+        value: T,
+        next: LinkedListNode<T> | null = null,
+        prev: LinkedListNode<T> | null = null,
+    ) {
+        this.value = value;
+        this.next = next;
+        this.prev = prev;
+    }
 }
 
 /**
  * Implementation of doubly linked list
  */
 export class LinkedList<T> implements Iterable<T> {
-	length: number;
-	head: LinkedListNode<T> | null;
-	tail: LinkedListNode<T> | null;
+    length: number;
+    head: LinkedListNode<T> | null;
+    tail: LinkedListNode<T> | null;
 
-	constructor(values?: Iterable<T>) {
-		this.length = 0;
-		this.head = this.tail = null;
-		if (!values) {
-			return;
-		}
-		for (const value of values) {
-			this.addLast(value);
-		}
-	}
+    constructor(values?: Iterable<T>) {
+        this.length = 0;
+        this.head = this.tail = null;
+        if (!values) {
+            return;
+        }
+        for (const value of values) {
+            this.addLast(value);
+        }
+    }
 
-	get(index: number) {
-		if (index < 0 || index >= this.length) {
-			return null;
-		}
+    get(index: number) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
 
-		let temp = this.head;
-		while (index > 0) {
-			if (!temp) {
-				return null;
-			}
-			temp = temp.next;
-			index--;
-		}
+        let temp = this.head;
+        while (index > 0) {
+            if (!temp) {
+                return null;
+            }
+            temp = temp.next;
+            index--;
+        }
 
-		if (!temp) {
-			return null;
-		}
-		return temp.value;
-	}
+        if (!temp) {
+            return null;
+        }
+        return temp.value;
+    }
 
-	addFirst(value: T): void {
-		let node = new LinkedListNode<T>(value, this.head);
+    addFirst(value: T): void {
+        let node = new LinkedListNode<T>(value, this.head);
 
-		if (this.head == null || this.tail == null) {
-			this.head = this.tail = node;
-			this.length = 1;
-			return;
-		}
+        if (this.head == null || this.tail == null) {
+            this.head = this.tail = node;
+            this.length = 1;
+            return;
+        }
 
-		this.head.prev = node;
-		this.head = node;
-		this.length++;
-	}
+        this.head.prev = node;
+        this.head = node;
+        this.length++;
+    }
 
-	addLast(value: T): void {
-		let node = new LinkedListNode<T>(value, null, this.tail);
+    addLast(value: T): void {
+        let node = new LinkedListNode<T>(value, null, this.tail);
 
-		if (this.head == null || this.tail == null) {
-			this.head = this.tail = node;
-			this.length = 1;
-			return;
-		}
+        if (this.head == null || this.tail == null) {
+            this.head = this.tail = node;
+            this.length = 1;
+            return;
+        }
 
-		this.tail.next = node;
-		this.tail = node;
-		this.length++;
-	}
+        this.tail.next = node;
+        this.tail = node;
+        this.length++;
+    }
 
-	deleteFirst() {
-		if (this.head == null) {
-			return null;
-		}
+    deleteFirst() {
+        if (this.head == null) {
+            return null;
+        }
 
-		const temp = this.head;
-		this.head = this.head.next;
-		this.length--;
-		return temp.value;
-	}
+        const temp = this.head;
+        this.head = this.head.next;
+        this.length--;
+        return temp.value;
+    }
 
-	deleteLast() {
-		if (this.tail == null) {
-			return null;
-		}
+    deleteLast() {
+        if (this.tail == null) {
+            return null;
+        }
 
-		const temp = this.tail;
-		this.tail = this.tail.prev;
-		this.length--;
-		return temp.value;
-	}
+        const temp = this.tail;
+        this.tail = this.tail.prev;
+        this.length--;
+        return temp.value;
+    }
 
-	delete(index: number) {
-		if (index < 0 || index >= this.length) {
-			return null;
-		}
+    delete(index: number) {
+        if (index < 0 || index >= this.length) {
+            return null;
+        }
 
-		let temp = this.head;
-		while (index > 1) {
-			temp = temp?.next ?? null;
-			index--;
-		}
-		this.length--;
-		return temp ?? null;
-	}
+        let temp = this.head;
+        while (index > 1) {
+            temp = temp?.next ?? null;
+            index--;
+        }
+        this.length--;
+        return temp ?? null;
+    }
+
 
 	reverse(): void {
 		let current = this.head;
@@ -134,30 +135,38 @@ export class LinkedList<T> implements Iterable<T> {
 		this.tail = temp;
 	}
 
-	[Symbol.iterator]() {
-		let temp = this.head;
-		const iterator: Iterator<T> = {
-			next(): IteratorResult<T> {
-				if (temp === null) {
-					return { value: undefined, done: true };
-				} else {
-					const value = temp.value;
-					temp = temp.next;
-					return { value, done: false };
-				}
-			},
-		};
-		return iterator;
-	}
+    forEach(callback: (value: T) => void): void {
+        let current = this.head;
+        while (current !== null) {
+            callback(current.value);
+            current = current.next;
+        }
+    }
 
-	toString(callback?: (value: T) => string, delimiter?: string): string {
-		let message = "HEAD" + (delimiter ?? " <-> ");
-		let temp: LinkedListNode<T> | null = this.head;
-		while (temp != null) {
-			const str = callback ? callback(temp.value) : temp.value;
-			message += str + (delimiter ?? " <-> ");
-			temp = temp.next;
-		}
-		return message + "END";
-	}
+    [Symbol.iterator]() {
+        let temp = this.head;
+        const iterator: Iterator<T> = {
+            next(): IteratorResult<T> {
+                if (temp === null) {
+                    return { value: undefined, done: true };
+                } else {
+                    const value = temp.value;
+                    temp = temp.next;
+                    return { value, done: false };
+                }
+            },
+        };
+        return iterator;
+    }
+
+    toString(callback?: (value: T) => string, delimiter?: string): string {
+        let message = "HEAD" + (delimiter ?? " <-> ");
+        let temp: LinkedListNode<T> | null = this.head;
+        while (temp != null) {
+            const str = callback ? callback(temp.value) : temp.value;
+            message += str + (delimiter ?? " <-> ");
+            temp = temp.next;
+        }
+        return message + "END";
+    }
 }
